2603f613076de879ab22e14b57ed2c2a8d5de682
weipengtao <wei_pengtao@qq.com>
2025-10-23 13:46:13 +0800

    feat: spring security 快速入门

---
+++ sample-02-01/src/main/java/com/security/Sample0201Application.java
@@ -0,0 +1,13 @@
+package com.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sample0201Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sample0201Application.class, args);
+    }
+
+}
